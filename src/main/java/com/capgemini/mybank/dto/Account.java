53a96cc316280bb30b47bf1ca0326f98dabb9c39
user <user@DESKTOP-2Q82ETD>
2020-02-27 13:19:29 +0530

    yashveer task 1 Completed Added Acount Class Date 27-Feb2020

---
+++ src/main/java/com/capgemini/mybank/dto/Account.java
@@ -0,0 +1,16 @@
+package com.capgemini.mybank.dto;
+
+public class Account {
+
+	private int accountId;
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	
+	
+}
